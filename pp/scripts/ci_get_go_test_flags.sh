ecfacda205a14516951c8ad9c6634b4d26cccf68
Vladimir Pustovalov <cherep@sura.ru>
2025-03-31 16:18:09 +0300

    changes after merge

--- pp/scripts/ci_get_go_test_flags.sh
+++ pp/scripts/ci_get_go_test_flags.sh
@@ -18,6 +18,7 @@ function append_tag_if_equal() {
 append_tag_if_equal "$OPT"        "dbg"             "dbg"
 append_tag_if_equal "$SANITIZERS" "with_sanitizers" "sanitize"
 append_tag_if_equal "$FASTCGO"    "without_fastcgo" "without_fastcgo"
+append_tag_if_equal "$STATIC"     "static"          "static"
 
 result=""
 if [ "$tags" != "" ]; then
