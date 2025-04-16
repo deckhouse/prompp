25054db53ddc95a980e87854aee7021620847822
glebshigin <gleb.shigin@flant.com>
2025-04-16 19:54:53 +0300

    revert from timeinterval to one timestamp

--- pp/series_data/querier/instant_querier.h
+++ pp/series_data/querier/instant_querier.h
@@ -7,42 +7,40 @@
 
 namespace series_data {
 class InstantQuerier {
-  using TimeInterval = PromPP::Primitives::TimeInterval;
+  using Timestamp = PromPP::Primitives::Timestamp;
   using LabelSetID = PromPP::Primitives::LabelSetID;
   using Sample = encoder::Sample;
   using ChunkType = chunk::DataChunk::Type;
 
- public:
-  PROMPP_ALWAYS_INLINE static void query_sample(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const TimeInterval& time_interval) noexcept {
+public:
+  PROMPP_ALWAYS_INLINE static void query_sample(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const Timestamp& timestamp) noexcept {
     if (storage.open_chunks.size() > ls_id) [[likely]] {
-      bool is_found = check_boundary(sample, storage, ls_id, time_interval);
+      bool is_found = check_boundary(sample, storage, ls_id, timestamp);
       if (!is_found) {
-        check_inside_series(sample, storage, ls_id, time_interval);
+        check_inside_series(sample, storage, ls_id, timestamp);
       }
     }
   }
 
- private:
-  static bool check_boundary(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const TimeInterval& time_interval) noexcept {
+private:
+  static bool check_boundary(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const Timestamp& timestamp) noexcept {
     const auto series_interval = Decoder::get_series_time_interval(storage, ls_id);
-    if (!series_interval.intersect(time_interval)) {
+    if (timestamp < series_interval.min) {
       return true;
     }
-    if (time_interval.contains(series_interval.max)) {
+    if (timestamp >= series_interval.max) {
       sample = {.timestamp = series_interval.max, .value = Decoder::get_open_chunk_last_value(storage, storage.open_chunks[ls_id])};
       return true;
     }
     return false;
   }
 
-  static bool check_inside_series(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const TimeInterval& time_interval) noexcept {
+  static bool check_inside_series(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const Timestamp& timestamp) noexcept {
     for (const auto& chunk_data : DataStorage::SeriesChunks(&storage, ls_id)) {
-      if (Decoder::get_chunk_time_interval(chunk_data).contains(time_interval.max)) {
+      if (Decoder::get_chunk_time_interval(chunk_data).contains(timestamp)) {
         Decoder::create_decode_iterator(chunk_data, [&](auto&& begin, auto&& end) {
-          for (auto sample_it = begin; sample_it != end && sample_it->timestamp <= time_interval.max; ++sample_it) {
-            if (time_interval.contains(sample_it->timestamp)) {
-              sample = *sample_it;
-            }
+          for (auto sample_it = begin; sample_it != end && sample_it->timestamp <= timestamp; ++sample_it) {
+            sample = *sample_it;
           }
         });
         return true;
@@ -51,4 +49,4 @@ class InstantQuerier {
     return false;
   }
 };
-}  // namespace series_data
+} // namespace series_data
