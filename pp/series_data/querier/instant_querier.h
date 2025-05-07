6fb789f0493e918cde57fa61490635871b0949a4
Vladimir Pustovalov <cherep@sura.ru>
2025-05-07 15:28:35 +0300

    fixed bug with empty open chunk in InstantQuerier

--- pp/series_data/querier/instant_querier.h
+++ pp/series_data/querier/instant_querier.h
@@ -14,7 +14,7 @@ class InstantQuerier {
 
  public:
   PROMPP_ALWAYS_INLINE static void query_sample(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const Timestamp& timestamp) noexcept {
-    if (storage.open_chunks.size() <= ls_id) [[unlikely]] {
+    if (storage.open_chunks.size() <= ls_id || storage.open_chunks[ls_id].is_empty()) [[unlikely]] {
       return;
     }
     if (const auto series_last_ts = Decoder::get_series_max_timestamp(storage, ls_id); timestamp >= series_last_ts) {
@@ -25,7 +25,7 @@ class InstantQuerier {
   }
 
  private:
-  static bool check_inside_series(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const Timestamp& timestamp) noexcept {
+  static void check_inside_series(Sample& sample, const DataStorage& storage, LabelSetID ls_id, const Timestamp& timestamp) noexcept {
     for (const auto& chunk_data : DataStorage::SeriesChunks(&storage, ls_id)) {
       if (Decoder::get_chunk_time_interval(chunk_data).contains(timestamp)) {
         Decoder::create_decode_iterator(chunk_data, [&](auto&& begin, auto&& end) PROMPP_LAMBDA_INLINE {
@@ -33,10 +33,8 @@ class InstantQuerier {
             sample = *sample_it;
           }
         });
-        return true;
       }
     }
-    return false;
   }
 };
 }  // namespace series_data
