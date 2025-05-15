857ca926d3ad9f815d436f8baed78d151871cf3b
Vladimir Pustovalov <cherep@sura.ru>
2025-05-15 11:56:25 +0300

    refactored RegexpMatchAnalyzer

--- pp/series_index/querier/regexp/match_analyzer.h
+++ pp/series_index/querier/regexp/match_analyzer.h
@@ -17,50 +17,22 @@ class RegexpMatchAnalyzer {
   };
 
   [[nodiscard]] static Status analyze(re2::Regexp* regexp) {
-    if (!regexp) {
+    if (!regexp) [[unlikely]] {
       return Status::kError;
     }
 
-    if (regexp->op() == re2::RegexpOp::kRegexpEmptyMatch) {
-      return Status::kEmptyMatch;
-    }
-
-    if (regexp->op() == re2::RegexpOp::kRegexpStar && regexp->sub()[0]->op() == re2::RegexpOp::kRegexpAnyChar) {
-      return Status::kAnythingMatch;
-    }
-
-    if (regexp->op() == re2::RegexpOp::kRegexpPlus && regexp->sub()[0]->op() == re2::RegexpOp::kRegexpAnyChar) {
-      return Status::kAllMatch;
-    }
-
     if (regexp->op() == re2::RegexpOp::kRegexpConcat) {
-      if (const auto i = skip_begin_text_anchor(regexp); i == skip_end_text_anchor(regexp, i)) {
-        if (regexp->sub()[i]->op() == re2::RegexpOp::kRegexpPlus) {
-          if (regexp->sub()[i]->sub()[0]->op() == re2::RegexpOp::kRegexpAnyChar) {
-            return Status::kAllMatch;
-          }
-        } else if (regexp->sub()[i]->op() == re2::RegexpOp::kRegexpStar) {
-          if (regexp->sub()[i]->sub()[0]->op() == re2::RegexpOp::kRegexpAnyChar) {
-            return Status::kAnythingMatch;
-          }
-        } else if (regexp->sub()[i]->op() == re2::RegexpOp::kRegexpEndText) {
-          return Status::kEmptyMatch;
-        } else if (regexp->sub()[i]->op() == re2::RegexpOp::kRegexpAlternate) {
-          if (has_empty_alternative(regexp->sub()[i])) {
-            return Status::kAllMatchWithExcludes;
-          }
-        }
+      if (const auto significant_sub_regexp = get_significant_sub_regexp(regexp); significant_sub_regexp) [[likely]] {
+        return analyze_sub_regexp(significant_sub_regexp);
       }
-    } else if (regexp->op() == re2::RegexpOp::kRegexpAlternate) {
-      if (has_empty_alternative(regexp)) {
-        return Status::kAllMatchWithExcludes;
-      }
+
+      return Status::kPartialMatch;
     }
 
-    return Status::kPartialMatch;
+    return analyze_sub_regexp(regexp);
   }
 
-  [[nodiscard]] static int skip_begin_text_anchor(re2::Regexp* regexp) noexcept {
+  [[nodiscard]] PROMPP_ALWAYS_INLINE static int skip_begin_text_anchor(re2::Regexp* regexp) noexcept {
     int i = 0;
     while (i < regexp->nsub() && regexp->sub()[i]->op() == re2::RegexpOp::kRegexpBeginText) {
       ++i;
@@ -69,7 +41,7 @@ class RegexpMatchAnalyzer {
     return i;
   }
 
-  [[nodiscard]] static int skip_end_text_anchor(re2::Regexp* regexp, int start) noexcept {
+  [[nodiscard]] PROMPP_ALWAYS_INLINE static int skip_end_text_anchor(re2::Regexp* regexp, int start) noexcept {
     int i = regexp->nsub() - 1;
     while (i > start && regexp->sub()[i]->op() == re2::RegexpOp::kRegexpEndText) {
       --i;
@@ -78,8 +50,47 @@ class RegexpMatchAnalyzer {
   }
 
  private:
-  [[nodiscard]] PROMPP_ALWAYS_INLINE static bool has_empty_alternative(re2::Regexp* regexp) noexcept {
+  [[nodiscard]] PROMPP_ALWAYS_INLINE static re2::Regexp* get_significant_sub_regexp(re2::Regexp* regexp) noexcept {
+    if (const auto i = skip_begin_text_anchor(regexp); i == skip_end_text_anchor(regexp, i)) {
+      return regexp->sub()[i];
+    }
+
+    return nullptr;
+  }
+
+  [[nodiscard]] static Status analyze_sub_regexp(re2::Regexp* regexp) noexcept {
+    if (BareBones::is_in(regexp->op(), re2::RegexpOp::kRegexpEmptyMatch, re2::RegexpOp::kRegexpEndText)) {
+      return Status::kEmptyMatch;
+    }
+
+    if (is_anything_match(regexp)) {
+      return Status::kAnythingMatch;
+    }
+
+    if (is_all_match(regexp)) {
+      return Status::kAllMatch;
+    }
+
+    if (has_empty_alternative(regexp)) {
+      return Status::kAllMatchWithExcludes;
+    }
+
+    return Status::kPartialMatch;
+  }
+
+  [[nodiscard]] PROMPP_ALWAYS_INLINE static bool is_anything_match(re2::Regexp* regexp) noexcept {
+    return regexp->op() == re2::RegexpOp::kRegexpStar && regexp->sub()[0]->op() == re2::RegexpOp::kRegexpAnyChar;
+  }
+  [[nodiscard]] PROMPP_ALWAYS_INLINE static bool is_all_match(re2::Regexp* regexp) noexcept {
+    return regexp->op() == re2::RegexpOp::kRegexpPlus && regexp->sub()[0]->op() == re2::RegexpOp::kRegexpAnyChar;
+  }
+
+  [[nodiscard]] static bool has_empty_alternative(re2::Regexp* regexp) noexcept {
     using enum re2::RegexpOp;
+
+    if (regexp->op() != kRegexpAlternate) {
+      return false;
+    }
 
     for (auto i = 0; i < regexp->nsub(); ++i) {
       if (const auto alternative = regexp->sub()[i]; alternative->nsub() == 0) {
