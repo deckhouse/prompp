8bc5616b295ca66a5378bb267feadca82ff51aa3
Alexandr Yudin <alexandr.yudin@flant.com>
2025-05-16 10:48:02 +0000

    for save

---
+++ model/labels/labels_common_noncpp.go
@@ -0,0 +1,22 @@
+// //go:build !cpplabels
+
+package labels
+
+// import "github.com/prometheus/prometheus/pp/go/cppbridge"
+
+// // NewLabelsCppWithLSS init LabelsCpp with LabelSetStorage and ls id.
+// func NewLabelsWithLSS(
+// 	lss *cppbridge.LabelSetStorage,
+// 	id uint32,
+// 	length uint16,
+// ) Labels {
+// 	if lss == nil {
+// 		return EmptyLabels()
+// 	}
+
+// 	return Labels{
+// 		lss:    lss,
+// 		id:     id,
+// 		length: length,
+// 	}
+// }
