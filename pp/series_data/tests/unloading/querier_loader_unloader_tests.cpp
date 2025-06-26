277f225f546ca9e231742193f44a05c6ba9ec173
glebshigin <gleb.shigin@flant.com>
2025-06-26 16:39:31 +0300

    querier and instant_querier small querier-loader-unloader tests

--- pp/series_data/tests/unloading/querier_loader_unloader_tests.cpp
+++ pp/series_data/tests/unloading/querier_loader_unloader_tests.cpp
@@ -5,10 +5,11 @@
 #include "series_data/data_storage.h"
 #include "series_data/encoder.h"
 #include "series_data/querier/instant_querier.h"
+#include "series_data/querier/querier.h"
 #include "series_data/unloading/loader.h"
 #include "series_data/unloading/unloader.h"
 
-class QuerierLoaderUnloaderTrait {
+class InstantQuerierLoaderUnloaderTrait {
  protected:
   series_data::DataStorage storage_;
   series_data::Encoder<> encoder_{storage_};
@@ -20,7 +21,7 @@ class QuerierLoaderUnloaderTrait {
   std::vector<uint32_t> indices_second_{0, 1, 2, 3, 4};
 };
 
-class QuerierLoaderUnloaderTestFixture : public QuerierLoaderUnloaderTrait, public testing::Test {
+class InstantQuerierLoaderUnloaderTestFixture : public InstantQuerierLoaderUnloaderTrait, public testing::Test {
  protected:
   void SetUp() override {
     storage_.reset();
@@ -37,7 +38,7 @@ class QuerierLoaderUnloaderTestFixture : public QuerierLoaderUnloaderTrait, publ
   }
 };
 
-TEST_F(QuerierLoaderUnloaderTestFixture, InstantQueryNeedLoading) {
+TEST_F(InstantQuerierLoaderUnloaderTestFixture, InstantQueryNeedLoading) {
   // Arrange
 
   // Act
@@ -71,7 +72,7 @@ TEST_F(QuerierLoaderUnloaderTestFixture, InstantQueryNeedLoading) {
   ASSERT_TRUE(storage_.unloaded_series_bitmap.contains(9));
 }
 
-TEST_F(QuerierLoaderUnloaderTestFixture, InstantQueryLoading) {
+TEST_F(InstantQuerierLoaderUnloaderTestFixture, InstantQueryLoading) {
   // Arrange
 
   // Act
@@ -101,4 +102,87 @@ TEST_F(QuerierLoaderUnloaderTestFixture, InstantQueryLoading) {
   ASSERT_TRUE(storage_.unloaded_series_bitmap.contains(5));
   ASSERT_TRUE(storage_.unloaded_series_bitmap.contains(7));
   ASSERT_TRUE(storage_.unloaded_series_bitmap.contains(9));
+}
+
+using Query = series_data::querier::Query<BareBones::Vector<PromPP::Primitives::LabelSetID>>;
+
+class QuerierLoaderUnloaderTrait {
+ protected:
+  series_data::DataStorage storage_;
+  series_data::Encoder<> encoder_{storage_};
+  BareBones::ShrinkedToFitOStringStream stream_;
+  series_data::unloading::Unloader unloader_{storage_};
+  series_data::querier::Querier querier_{storage_};
+
+  PromPP::Primitives::TimeInterval interval_{.min = 1, .max = 3};
+  PromPP::Primitives::TimeInterval reload_interval_{.min = 1, .max = 5};
+};
+
+class QuerierLoaderUnloaderTestFixture : public QuerierLoaderUnloaderTrait, public ::testing::Test {
+ protected:
+  void SetUp() override {
+    storage_.reset();
+    stream_.clear();
+
+    uint32_t idx = 0;
+    for (uint32_t ls_id = 0; ls_id < 5; ++ls_id) {
+      encoder_.encode(ls_id, 1, idx++);
+      encoder_.encode(ls_id, 2, idx++);
+      encoder_.encode(ls_id, 3, idx++);
+      encoder_.encode(ls_id, 4, idx++);
+      encoder_.encode(ls_id, 5, idx++);
+    }
+  }
+};
+
+TEST_F(QuerierLoaderUnloaderTestFixture, QuerierNeedLoading) {
+  // Arrange
+  Query query1{.time_interval = interval_, .label_set_ids = {0, 2, 4}};
+  Query query2{.time_interval = interval_, .label_set_ids = {1, 2, 3}};
+
+  // Act
+  std::ignore = querier_.query(query1);
+
+  unloader_.unload(stream_);
+
+  const auto& chunks_after = querier_.query(query2);
+
+  // Assert
+  EXPECT_EQ(chunks_after.size(), 3);
+
+  EXPECT_TRUE(querier_.need_loading());
+
+  EXPECT_EQ(querier_.get_series_to_load().cardinality(), 2);
+  EXPECT_TRUE(querier_.get_series_to_load().contains(1));
+  EXPECT_TRUE(querier_.get_series_to_load().contains(3));
+
+  EXPECT_EQ(storage_.queried_series_bitmap.cardinality(), 5);
+
+  EXPECT_EQ(storage_.unloaded_series_bitmap.cardinality(), 2);
+
+  EXPECT_TRUE(storage_.unloaded_series_bitmap.contains(1));
+  EXPECT_TRUE(storage_.unloaded_series_bitmap.contains(3));
+}
+
+TEST_F(QuerierLoaderUnloaderTestFixture, QuerierLoad) {
+  // Arrange
+  Query query1{.time_interval = interval_, .label_set_ids = {0, 2, 4}};
+  Query query2{.time_interval = reload_interval_, .label_set_ids = {0, 1, 2}};
+
+  // Act
+  std::ignore = querier_.query(query1);
+
+  unloader_.unload(stream_);
+
+  std::ignore = querier_.query(query2);
+
+  series_data::unloading::Loader loader(storage_, querier_);
+  loader.load_next(stream_.span<const uint8_t>());
+  loader.load_finalize();
+
+  // Assert
+  EXPECT_EQ(storage_.queried_series_bitmap.cardinality(), 4);
+
+  EXPECT_EQ(storage_.unloaded_series_bitmap.cardinality(), 1);
+  EXPECT_TRUE(storage_.unloaded_series_bitmap.contains(3));
 }
