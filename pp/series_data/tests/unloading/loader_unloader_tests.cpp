0c64b13600fd8afdabbdd7f7d2054425aa05235b
glebshigin <gleb.shigin@flant.com>
2025-07-17 19:40:18 +0300

    Unloader tests better coverage

--- pp/series_data/tests/unloading/loader_unloader_tests.cpp
+++ pp/series_data/tests/unloading/loader_unloader_tests.cpp
@@ -13,13 +13,14 @@ using series_data::Decoder;
 using series_data::chunk::DataChunk;
 using series_data::encoder::SampleList;
 using series_data::unloading::Loader;
+using series_data::unloading::Unloader;
 using std::operator""s;
 
 class LoaderUnloaderTrait {
  protected:
   series_data::DataStorage storage_;
   series_data::Encoder<> encoder_{storage_};
-  series_data::unloading::Unloader unloader_{storage_};
+  Unloader unloader_{storage_};
   BareBones::ShrinkedToFitOStringStream stream1_;
   BareBones::ShrinkedToFitOStringStream stream2_;
 };
@@ -406,6 +407,68 @@ TEST_F(LoaderUnloaderTestFixture, LoadValuesGorillaChunk) {
   ASSERT_EQ(series_data::EncodingType::kValuesGorilla, storage_.open_chunks[1].encoding_state.encoding_type);
   ASSERT_EQ((SampleList{{1, 1.1}, {2, 2.0}, {3, 3.0}}), Decoder::decode_chunk<DataChunk::Type::kOpen>(storage_, storage_.open_chunks[1]));
   ASSERT_FALSE(storage_.unloaded_series_bitmap.is_set(1));
+}
+
+class LoaderUnloaderBigTestFixture : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    encoder_.encode(0, 1, 0.0);
+    encoder_.encode(0, 2, 0.0);
+    encoder_.encode(0, 3, 0.0);
+    encoder_.encode(0, 4, 0.0);
+    encoder_.encode(0, 5, 0.0);
+
+    encoder_.encode(1, 1, 1.0);
+    encoder_.encode(1, 2, 1.0);
+    encoder_.encode(1, 3, 1.0);
+    encoder_.encode(1, 4, 1.0);
+    encoder_.encode(1, 5, 1.0);
+
+    encoder_.encode(2, 1, 1.0);
+    encoder_.encode(2, 2, 2.0);
+    encoder_.encode(2, 3, 3.0);
+    encoder_.encode(2, 4, 4.0);
+    encoder_.encode(2, 5, 5.0);
+
+    encoder_.encode(3, 1, 6.0);
+    encoder_.encode(3, 2, 7.0);
+    encoder_.encode(3, 3, 8.0);
+    encoder_.encode(3, 4, 9.0);
+    encoder_.encode(3, 5, 10.0);
+  }
+
+  series_data::DataStorage storage_;
+  series_data::Encoder<> encoder_{storage_};
+  BareBones::ShrinkedToFitOStringStream stream_;
+};
+
+TEST_F(LoaderUnloaderBigTestFixture, UnloadAfterQuery) {
+  // Arrange
+  storage_.queried_series_bitmap.set({0, 1, 3});
+  const uint32_t chunk_stream_size_in_bits =
+      storage_.get_asc_integer_stream<DataChunk::Type::kOpen>(storage_.open_chunks[2].encoder.external_index).size_in_bits();
+
+  // Act
+  Unloader(storage_).unload(stream_);
+
+  // Assert
+  ASSERT_EQ(storage_.get_asc_integer_stream<DataChunk::Type::kOpen>(storage_.open_chunks[2].encoder.external_index).size_in_bits(),
+            chunk_stream_size_in_bits % 8);
+
+  ASSERT_EQ(storage_.unloaded_series_bitmap.popcount(), 1);
+  ASSERT_TRUE(storage_.unloaded_series_bitmap.is_set(2));
+}
+
+TEST_F(LoaderUnloaderBigTestFixture, NothingToUnload) {
+  // Arrange
+  storage_.queried_series_bitmap.set({2, 3});
+
+  // Act
+  Unloader(storage_).unload(stream_);
+
+  // Assert
+  ASSERT_TRUE(storage_.unloaded_series_bitmap.empty());
+  ASSERT_EQ(stream_.view(), "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"s);
 }
 
 }  // namespace
