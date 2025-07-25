f609eaa173685f790cf24620597a542ebd7a7b5f
glebshigin <gleb.shigin@flant.com>
2025-07-25 15:37:24 +0300

    UnorderedVector fixes

--- pp/series_data/unloading/loader.h
+++ pp/series_data/unloading/loader.h
@@ -17,6 +17,11 @@ namespace series_data::unloading {
 struct PROMPP_ATTRIBUTE_PACKED SeriesToLoadInfo {
   uint8_t chunk_id = 0;
   encoder::CompactBitSequence buffer{};
+
+  void reset() noexcept {
+    chunk_id = 0;
+    buffer.rewind();
+  }
 };
 }  // namespace series_data::unloading
 
@@ -25,6 +30,7 @@ struct BareBones::IsTriviallyReallocatable<series_data::unloading::SeriesToLoadI
 
 namespace series_data::unloading {
 class Loader {
+ public:
   class UnorderedVector {
    public:
     [[nodiscard]] PROMPP_ALWAYS_INLINE bool empty() const noexcept { return ls_id_to_offset_.empty(); }
@@ -39,35 +45,13 @@ class Loader {
     PROMPP_ALWAYS_INLINE void clear() noexcept {
       ls_id_to_offset_.erase(ls_id_to_offset_.begin(), ls_id_to_offset_.end());
       for (auto& info : series_to_load_infos_) {
-        info.chunk_id = 0;
-        info.buffer.rewind();
+        info.reset();
       }
     }
 
-    [[nodiscard]] PROMPP_ALWAYS_INLINE SeriesToLoadInfo& operator[](uint32_t key) noexcept {
-      if (auto it = ls_id_to_offset_.find(key); it != ls_id_to_offset_.end()) [[likely]] {
-        return series_to_load_infos_[it->second];
-      }
-
-      ls_id_to_offset_[key] = ls_id_to_offset_.size();
-
-      if (ls_id_to_offset_.size() == series_to_load_infos_.size()) [[unlikely]] {
-        return series_to_load_infos_.emplace_back();
-      }
-      return series_to_load_infos_.back();
-    }
-
-    [[nodiscard]] PROMPP_ALWAYS_INLINE const SeriesToLoadInfo& operator[](uint32_t key) const noexcept {
-      assert(ls_id_to_offset_.find(key) != ls_id_to_offset_.end());
-      return series_to_load_infos_[ls_id_to_offset_.find(key)->second];
-    }
-
-    class IteratorSentinel {};
-    template <bool IsConst>
+    template <class MapIterator, class Vector>
     class Iterator {
-      using MapIterator = phmap::flat_hash_map<uint32_t, uint32_t>::const_iterator;
-      using VectorPtr = std::conditional_t<IsConst, const UnorderedVector*, UnorderedVector*>;
-      using RefType = std::conditional_t<IsConst, const SeriesToLoadInfo&, SeriesToLoadInfo&>;
+      using RefType = typename Vector::value_type&;
       using PairType = std::pair<int, RefType>;
 
      public:
@@ -76,9 +60,9 @@ class Loader {
       using difference_type = std::ptrdiff_t;
 
       PROMPP_ALWAYS_INLINE Iterator() noexcept = default;
-      PROMPP_ALWAYS_INLINE Iterator(MapIterator map_it, VectorPtr parent) noexcept : map_it_(map_it), parent_(parent) {}
+      PROMPP_ALWAYS_INLINE Iterator(MapIterator map_it, Vector* parent) noexcept : map_it_(map_it), vector_ptr_(parent) {}
 
-      PROMPP_ALWAYS_INLINE PairType operator*() const noexcept { return {map_it_->first, parent_->series_to_load_infos_[map_it_->second]}; }
+      PROMPP_ALWAYS_INLINE PairType operator*() const noexcept { return {map_it_->first, vector_ptr_->operator[](map_it_->second)}; }
 
       PROMPP_ALWAYS_INLINE Iterator& operator++() noexcept {
         ++map_it_;
@@ -91,49 +75,45 @@ class Loader {
         return retval;
       }
 
-      PROMPP_ALWAYS_INLINE bool operator==(const Iterator& other) const { return map_it_ == other.map_it_; }
-      PROMPP_ALWAYS_INLINE bool operator==(const IteratorSentinel&) const { return map_it_ == parent_->ls_id_to_offset_.end(); }
+      PROMPP_ALWAYS_INLINE bool operator==(const Iterator& other) const { return vector_ptr_ == other.vector_ptr_ && map_it_ == other.map_it_; }
 
      private:
       MapIterator map_it_{};
-      VectorPtr parent_{nullptr};
+      Vector* vector_ptr_{nullptr};
     };
-    using const_iterator = Iterator<true>;
-    using iterator = Iterator<false>;
-    using sentinel = IteratorSentinel;
 
-    [[nodiscard]] PROMPP_ALWAYS_INLINE iterator begin() noexcept { return {ls_id_to_offset_.begin(), this}; }
-    [[nodiscard]] PROMPP_ALWAYS_INLINE const_iterator begin() const noexcept { return {ls_id_to_offset_.begin(), this}; }
+    [[nodiscard]] PROMPP_ALWAYS_INLINE auto begin() noexcept { return Iterator{ls_id_to_offset_.begin(), &series_to_load_infos_}; }
+    [[nodiscard]] PROMPP_ALWAYS_INLINE auto begin() const noexcept { return Iterator{ls_id_to_offset_.begin(), &series_to_load_infos_}; }
 
-    [[nodiscard]] static PROMPP_ALWAYS_INLINE sentinel end() noexcept { return {}; }
+    [[nodiscard]] PROMPP_ALWAYS_INLINE auto end() noexcept { return Iterator{ls_id_to_offset_.end(), &series_to_load_infos_}; }
+    [[nodiscard]] PROMPP_ALWAYS_INLINE auto end() const noexcept { return Iterator{ls_id_to_offset_.end(), &series_to_load_infos_}; }
 
-    PROMPP_ALWAYS_INLINE iterator find(uint32_t key) noexcept {
+    PROMPP_ALWAYS_INLINE auto find(uint32_t key) noexcept {
       if (auto it = ls_id_to_offset_.find(key); it != ls_id_to_offset_.end()) [[likely]] {
-        return {it, this};
+        return Iterator{it, &series_to_load_infos_};
       }
-      return {ls_id_to_offset_.end(), this};
-    }
-    [[nodiscard]] PROMPP_ALWAYS_INLINE const_iterator find(uint32_t key) const noexcept {
-      if (auto it = ls_id_to_offset_.find(key); it != ls_id_to_offset_.end()) [[likely]] {
-        return {it, this};
-      }
-      return {ls_id_to_offset_.end(), this};
+      return Iterator{ls_id_to_offset_.end(), &series_to_load_infos_};
     }
 
-    PROMPP_ALWAYS_INLINE iterator insert(uint32_t key) noexcept {
+    PROMPP_ALWAYS_INLINE auto find(uint32_t key) const noexcept {
+      if (auto it = ls_id_to_offset_.find(key); it != ls_id_to_offset_.end()) [[likely]] {
+        return Iterator{it, &series_to_load_infos_};
+      }
+      return Iterator{ls_id_to_offset_.end(), &series_to_load_infos_};
+    }
+
+    PROMPP_ALWAYS_INLINE auto insert(uint32_t key) noexcept {
       if (const auto it = find(key); it != end()) [[unlikely]] {
-        auto& [chunk_id, buffer] = (*it).second;
-        chunk_id = 0;
-        buffer.rewind();
+        (*it).second.reset();
         return it;
       }
 
-      auto map_it = ls_id_to_offset_.insert({key, ls_id_to_offset_.size()});
       if (ls_id_to_offset_.size() == series_to_load_infos_.size()) [[unlikely]] {
         series_to_load_infos_.emplace_back();
       }
+      auto map_it = ls_id_to_offset_.insert({key, ls_id_to_offset_.size()});
 
-      return {map_it.first, this};
+      return Iterator{map_it.first, &series_to_load_infos_};
     }
 
    private:
@@ -141,7 +121,6 @@ class Loader {
     phmap::flat_hash_map<uint32_t, uint32_t> ls_id_to_offset_{};
   };
 
- public:
   explicit Loader(DataStorage& storage) : storage_(storage) {
     ls_id_to_infos_.reserve(storage_.unloaded_series_bitmap.popcount());
 
