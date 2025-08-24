840df371e9e91a226f3ec89199873cb869108bb4
Bastrykov Evgeniy <vporoshok@gmail.com>
2025-08-24 11:39:34 +0400

    review

--- web/web_fuzzy_test.go
+++ web/web_fuzzy_test.go
@@ -1,16 +1,3 @@
-// Copyright 2015 The Prometheus Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package web_test
 
 import (
@@ -68,7 +55,6 @@ func FuzzWeb(f *testing.F) {
 	}, 5*time.Second, 100*time.Millisecond, "Server hasn't start")
 
 	f.Fuzz(func(t *testing.T, query string, body []byte) {
-		// func(t TestingT, query string, body []byte) {
 		t.Log("fuzzing")
 		type Endpoint struct {
 			path    string
@@ -98,7 +84,6 @@ func FuzzWeb(f *testing.F) {
 			}
 		}
 	})
-	// }(f, "lkasjdf", []byte{12, 25, 234})
 }
 
 type TestingT interface {
