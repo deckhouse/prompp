7ae736abb73bcd6fcfe8233b682d89b22d610d91
Gleb Shigin <gleb.shigin@gmail.com>
2025-08-25 17:13:16 +0300

    Added tokenizer traverse benchmark

--- pp/wal/benchmarks/scraper_benchmark.cpp
+++ pp/wal/benchmarks/scraper_benchmark.cpp
@@ -7,7 +7,30 @@
 
 namespace {
 
+using PromPP::WAL::hashdex::scraper::PrometheusParser;
 using PromPP::WAL::hashdex::scraper::PrometheusScraper;
+
+void BenchmarkParser(benchmark::State& state) {
+  constexpr auto get_file_name = [] -> std::string {
+    if (auto& context = benchmark::internal::GetGlobalContext(); context != nullptr) {
+      return context->operator[]("scraper_file");
+    }
+
+    return {};
+  };
+
+  std::ifstream t(get_file_name());
+  std::string str((std::istreambuf_iterator(t)), std::istreambuf_iterator<char>());
+
+  PrometheusParser parser;
+
+  for ([[maybe_unused]] auto _ : state) {
+    parser.tokenizer().tokenize(str);
+    while (parser.tokenizer().next() != PromPP::Prometheus::textparse::Token::kEOF) {
+      benchmark::DoNotOptimize(parser);
+    }
+  }
+}
 
 void BenchmarkScraperParse(benchmark::State& state) {
   constexpr auto get_file_name = [] -> std::string {
@@ -32,7 +55,6 @@ void BenchmarkScraperParse(benchmark::State& state) {
   }
 
   state.counters["Alloc"] = benchmark::Counter(allocated_memory, benchmark::Counter::kDefaults, benchmark::Counter::OneK::kIs1024);
-  ;
 }
 
 void BenchmarkScraperRead(benchmark::State& state) {
@@ -58,6 +80,7 @@ void BenchmarkScraperRead(benchmark::State& state) {
   }
 }
 
+BENCHMARK(BenchmarkParser)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
 BENCHMARK(BenchmarkScraperParse)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
 BENCHMARK(BenchmarkScraperRead)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
 
