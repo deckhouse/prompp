4656f7137066bf8f873c12459655f1f4a6aa4599
Gleb Shigin <gleb.shigin@gmail.com>
2025-08-29 15:19:09 +0300

    initial encoding

--- pp/wal/benchmarks/scraper_benchmark.cpp
+++ pp/wal/benchmarks/scraper_benchmark.cpp
@@ -10,27 +10,27 @@ namespace {
 using PromPP::WAL::hashdex::scraper::PrometheusParser;
 using PromPP::WAL::hashdex::scraper::PrometheusScraper;
 
-void BenchmarkParser(benchmark::State& state) {
-  constexpr auto get_file_name = [] -> std::string {
-    if (auto& context = benchmark::internal::GetGlobalContext(); context != nullptr) {
-      return context->operator[]("scraper_file");
-    }
-
-    return {};
-  };
-
-  std::ifstream t(get_file_name());
-  std::string str((std::istreambuf_iterator(t)), std::istreambuf_iterator<char>());
-
-  PrometheusParser parser;
-
-  for ([[maybe_unused]] auto _ : state) {
-    parser.tokenizer().tokenize(str);
-    while (parser.tokenizer().next() != PromPP::Prometheus::textparse::Token::kEOF) {
-      benchmark::DoNotOptimize(parser);
-    }
-  }
-}
+// void BenchmarkParser(benchmark::State& state) {
+//   constexpr auto get_file_name = [] -> std::string {
+//     if (auto& context = benchmark::internal::GetGlobalContext(); context != nullptr) {
+//       return context->operator[]("scraper_file");
+//     }
+//
+//     return {};
+//   };
+//
+//   std::ifstream t(get_file_name());
+//   std::string str((std::istreambuf_iterator(t)), std::istreambuf_iterator<char>());
+//
+//   PrometheusParser parser;
+//
+//   for ([[maybe_unused]] auto _ : state) {
+//     parser.tokenizer().tokenize(str);
+//     while (parser.tokenizer().next() != PromPP::Prometheus::textparse::Token::kEOF) {
+//       benchmark::DoNotOptimize(parser);
+//     }
+//   }
+// }
 
 void BenchmarkScraperParse(benchmark::State& state) {
   constexpr auto get_file_name = [] -> std::string {
@@ -41,47 +41,48 @@ void BenchmarkScraperParse(benchmark::State& state) {
     return {};
   };
 
-  size_t allocated_memory = 0;
-
   std::ifstream t(get_file_name());
   std::string str((std::istreambuf_iterator(t)), std::istreambuf_iterator<char>());
 
-  PrometheusScraper scraper;
-
   for ([[maybe_unused]] auto _ : state) {
+    PrometheusScraper scraper;
     auto tmp_str = str;
     std::ignore = scraper.parse(tmp_str, 0);
-    allocated_memory = scraper.allocated_memory();
   }
 
-  state.counters["Alloc"] = benchmark::Counter(allocated_memory, benchmark::Counter::kDefaults, benchmark::Counter::OneK::kIs1024);
-}
-
-void BenchmarkScraperRead(benchmark::State& state) {
-  constexpr auto get_file_name = [] -> std::string {
-    if (auto& context = benchmark::internal::GetGlobalContext(); context != nullptr) {
-      return context->operator[]("scraper_file");
-    }
-
-    return {};
-  };
-
-  std::ifstream t(get_file_name());
-  std::string str((std::istreambuf_iterator(t)), std::istreambuf_iterator<char>());
-
-  PrometheusScraper scraper;
-  std::ignore = scraper.parse(str, 0);
-
-  for ([[maybe_unused]] auto _ : state) {
-    PromPP::Primitives::TimeseriesSemiview ts;
-    for (auto& metric : scraper.metrics()) {
-      metric.read(ts);
-    }
+  {
+    PrometheusScraper scraper;
+    auto tmp_str = str;
+    std::ignore = scraper.parse(tmp_str, 0);
+    state.counters["Alloc"] = benchmark::Counter(scraper.allocated_memory(), benchmark::Counter::kDefaults, benchmark::Counter::OneK::kIs1024);
   }
 }
 
-BENCHMARK(BenchmarkParser)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
+// void BenchmarkScraperRead(benchmark::State& state) {
+//   constexpr auto get_file_name = [] -> std::string {
+//     if (auto& context = benchmark::internal::GetGlobalContext(); context != nullptr) {
+//       return context->operator[]("scraper_file");
+//     }
+//
+//     return {};
+//   };
+//
+//   std::ifstream t(get_file_name());
+//   std::string str((std::istreambuf_iterator(t)), std::istreambuf_iterator<char>());
+//
+//   PrometheusScraper scraper;
+//   std::ignore = scraper.parse(str, 0);
+//
+//   for ([[maybe_unused]] auto _ : state) {
+//     PromPP::Primitives::TimeseriesSemiview ts;
+//     for (auto& metric : scraper.metrics()) {
+//       metric.read(ts);
+//     }
+//   }
+// }
+
+// BENCHMARK(BenchmarkParser)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
 BENCHMARK(BenchmarkScraperParse)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
-BENCHMARK(BenchmarkScraperRead)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
+// BENCHMARK(BenchmarkScraperRead)->ComputeStatistics("min", [](const std::vector<double>& v) { return *std::ranges::min_element(v); });
 
 }  // namespace
