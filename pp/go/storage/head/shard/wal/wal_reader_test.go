9ced693efb4d3a20d128d1bfbcb157c7c8991277
Alexandr Yudin <alexandr.yudin@flant.com>
2025-09-24 06:11:47 +0000

    add wal reader test

---
+++ pp/go/storage/head/shard/wal/wal_reader_test.go
@@ -0,0 +1,80 @@
+package wal_test
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/prometheus/prometheus/pp/go/storage/head/shard/wal"
+	"github.com/stretchr/testify/suite"
+)
+
+type SegmentWalReaderSuite struct {
+	suite.Suite
+}
+
+func TestSegmentWalReaderSuite(t *testing.T) {
+	suite.Run(t, new(SegmentWalReaderSuite))
+}
+
+func (s *SegmentWalReaderSuite) TestHappyPath() {
+	buf := &bytes.Buffer{}
+	data := []byte(faker.Paragraph())
+	data = data[:(len(data)/10)*10]
+	_, err := buf.Write(data)
+	s.Require().NoError(err)
+
+	swr := wal.NewSegmentWalReader(buf, newTestSegment)
+	limiter := 0
+	actual := make([]byte, 0, len(data))
+	err = swr.ForEachSegment(func(rsm *testSegment) error {
+		actual = append(actual, rsm.Bytes()...)
+
+		limiter++
+		if limiter == 1000 {
+			return errors.New("limiter")
+		}
+		return nil
+	})
+	s.Require().NoError(err)
+
+	s.Equal(data, actual)
+}
+
+//
+// testSegment
+//
+
+// testSegment implements [ReadSegment].
+type testSegment struct {
+	buf []byte
+	*ReadSegmentMock
+}
+
+// newTestSegment init new [testSegment].
+func newTestSegment() *testSegment {
+	s := &testSegment{
+		buf: make([]byte, 10),
+	}
+
+	s.ReadSegmentMock = &ReadSegmentMock{
+		ReadFromFunc: func(r io.Reader) (int64, error) {
+			n, err := io.ReadFull(r, s.buf)
+			return int64(n), err
+		},
+		ResetFunc: func() {
+			for i := range s.buf {
+				s.buf[i] = 0
+			}
+		},
+	}
+
+	return s
+}
+
+// Bytes returns data.
+func (s *testSegment) Bytes() []byte {
+	return s.buf
+}
