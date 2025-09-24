75618fe1a2a5e76b2050cfbda18c640fde04219c
cherep58 <cherep@sura.ru>
2025-09-24 12:17:50 +0300

    Head keeper (#154)
    * created HeadConvertingQueue
    
    * added timeInterval cache for HeadDataStorage
    
    * rewrite Keeper
    
    * changes in cppbridge/relabeler
    
    * added data loading/unloading
    
    * created unit tests for block/writer
    
    * created persistener
    
    * fixed data race in LoadAndQuerySeriesDataTask
    
    * fixed compilation error
    
    * added mock
    
    * fixed compilation error
    
    * fix adapter

--- pp/go/storage/block/block_writer.go
+++ pp/go/storage/block/block_writer.go
@@ -275,6 +275,315 @@ func newChunkRecoder(chunkIterator ChunkIterator) chunkRecoder {
 	}
 }
 
+<<<<<<<< HEAD:pp/go/storage/block/block_writer.go
+========
+type WrittenBlock struct {
+	Dir  string
+	Meta tsdb.BlockMeta
+}
+
+func (block *WrittenBlock) ChunkDir() string {
+	return filepath.Join(block.Dir, "chunks")
+}
+
+func (block *WrittenBlock) IndexFilename() string {
+	return filepath.Join(block.Dir, indexFilename)
+}
+
+func (block *WrittenBlock) MetaFilename() string {
+	return filepath.Join(block.Dir, metaFilename)
+}
+
+type blockWriter struct {
+	WrittenBlock
+
+	chunkWriter     *ChunkWriter
+	indexFileWriter *FileWriter
+	indexWriter     IndexWriter
+
+	chunkRecoder chunkRecoder
+}
+
+func newBlockWriter(dir string, maxBlockChunkSegmentSize int64, indexWriter IndexWriter, chunkIterator ChunkIterator) (writer blockWriter, err error) {
+	uid := ulid.MustNew(ulid.Now(), rand.Reader)
+	writer.Dir = filepath.Join(dir, uid.String()) + tmpForCreationBlockDirSuffix
+
+	if err = createTmpDir(writer.Dir); err != nil {
+		return
+	}
+
+	if err = writer.createWriters(maxBlockChunkSegmentSize); err != nil {
+		return
+	}
+
+	writer.Meta = tsdb.BlockMeta{
+		ULID:    uid,
+		MinTime: math.MaxInt64,
+		MaxTime: math.MinInt64,
+		Version: metaVersion1,
+		Compaction: tsdb.BlockMetaCompaction{
+			Level:   1,
+			Sources: []ulid.ULID{uid},
+		},
+	}
+
+	writer.indexWriter = indexWriter
+	writer.chunkRecoder = newChunkRecoder(chunkIterator)
+	return
+}
+
+func (writer *blockWriter) createWriters(maxBlockChunkSegmentSize int64) error {
+	chunkWriter, err := NewChunkWriter(writer.ChunkDir(), maxBlockChunkSegmentSize)
+	if err != nil {
+		return fmt.Errorf("failed to create chunk writer: %w", err)
+	}
+
+	indexFileWriter, err := NewFileWriter(writer.IndexFilename())
+	if err != nil {
+		_ = chunkWriter.Close()
+		return fmt.Errorf("failed to create index file writer: %w", err)
+	}
+
+	writer.chunkWriter = chunkWriter
+	writer.indexFileWriter = indexFileWriter
+	return nil
+}
+
+func (writer *blockWriter) Close() error {
+	return closeAll(writer.chunkWriter, writer.indexFileWriter)
+}
+
+func (writer *blockWriter) RecodeAndWriteChunksBatch() error {
+	return writer.chunkRecoder.Recode(writer.chunkWriter, &writer.Meta, writer.writeSeries)
+}
+
+func (writer *blockWriter) WriteRestOfRecodedChunks() error {
+	return writer.writeSeries(writer.chunkRecoder.previousSeriesID, writer.chunkRecoder.chunksMetadata)
+}
+
+func (writer *blockWriter) writeSeries(seriesID uint32, chunksMetadata []ChunkMetadata) error {
+	if len(chunksMetadata) > 0 {
+		if _, err := writer.indexWriter.WriteSeriesTo(seriesID, chunksMetadata, writer.indexFileWriter); err != nil {
+			return fmt.Errorf("failed to write series %d: %w", seriesID, err)
+		}
+	}
+
+	return nil
+}
+
+func (writer *blockWriter) writeIndex() error {
+	if _, err := writer.indexWriter.WriteRestTo(writer.indexFileWriter); err != nil {
+		return fmt.Errorf("failed to write index: %w", err)
+	}
+
+	writer.Meta.MaxTime += 1
+	if _, err := writeBlockMetaFile(writer.MetaFilename(), &writer.Meta); err != nil {
+		return fmt.Errorf("failed to write block meta file: %w", err)
+	}
+
+	return nil
+}
+
+func (writer *blockWriter) MoveTmpDirToDir() error {
+	if err := syncDir(writer.Dir); err != nil {
+		return fmt.Errorf("failed to sync temporary block dir: %w", err)
+	}
+
+	dir := writer.Dir[:len(writer.Dir)-len(tmpForCreationBlockDirSuffix)]
+
+	if err := fileutil.Replace(writer.Dir, dir); err != nil {
+		return fmt.Errorf("failed to move temporary block dir {%s} to {%s}: %w", writer.Dir, dir, err)
+	}
+
+	writer.Dir = dir
+	return nil
+}
+
+type blockWriters struct {
+	writers []blockWriter
+}
+
+func (bw *blockWriters) append(writer blockWriter) {
+	bw.writers = append(bw.writers, writer)
+}
+
+func (bw *blockWriters) close() {
+	for i := range bw.writers {
+		_ = bw.writers[i].Close()
+	}
+}
+
+func (bw *blockWriters) recodeAndWriteChunksBatch() error {
+	for i := range bw.writers {
+		if err := bw.writers[i].RecodeAndWriteChunksBatch(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (bw *blockWriters) writeRestOfRecodedChunks() error {
+	for i := range bw.writers {
+		if err := bw.writers[i].WriteRestOfRecodedChunks(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (bw *blockWriters) writeIndexAndMoveTmpDirToDir() ([]WrittenBlock, error) {
+	writtenBlocks := make([]WrittenBlock, 0, len(bw.writers))
+	for i := range bw.writers {
+		if err := bw.writers[i].writeIndex(); err != nil {
+			return nil, err
+		}
+
+		if err := bw.writers[i].MoveTmpDirToDir(); err != nil {
+			return nil, err
+		}
+
+		writtenBlocks = append(writtenBlocks, bw.writers[i].WrittenBlock)
+	}
+
+	return writtenBlocks, nil
+}
+
+type Writer struct {
+	dataDir                  string
+	maxBlockChunkSegmentSize int64
+	blockDurationMs          int64
+	blockWriteDuration       *prometheus.GaugeVec
+}
+
+func NewWriter(
+	dataDir string,
+	maxBlockChunkSegmentSize int64,
+	blockDuration time.Duration,
+	registerer prometheus.Registerer,
+) *Writer {
+	factory := util.NewUnconflictRegisterer(registerer)
+	return &Writer{
+		dataDir:                  dataDir,
+		maxBlockChunkSegmentSize: maxBlockChunkSegmentSize,
+		blockDurationMs:          blockDuration.Milliseconds(),
+		blockWriteDuration: factory.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "prompp_block_write_duration",
+			Help: "Block write duration in milliseconds.",
+		}, []string{"block_id"}),
+	}
+}
+
+func (w *Writer) Write(shard relabeler.Shard) ([]WrittenBlock, error) {
+	shard.LSSRLock()
+	defer shard.LSSRUnlock()
+
+	writers, err := w.createWriters(shard)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		writers.close()
+	}()
+
+	if err = w.recodeAndWriteChunks(shard, writers); err != nil {
+		return nil, err
+	}
+
+	return writers.writeIndexAndMoveTmpDirToDir()
+}
+
+func (w *Writer) createWriters(shard relabeler.Shard) (blockWriters, error) {
+	var writers blockWriters
+
+	shard.DataStorageRLock()
+	timeInterval := shard.DataStorage().TimeInterval(false)
+	shard.DataStorageRUnlock()
+
+	quantStart := (timeInterval.MinT / w.blockDurationMs) * w.blockDurationMs
+	for ; quantStart <= timeInterval.MaxT; quantStart += w.blockDurationMs {
+		minT, maxT := quantStart, quantStart+w.blockDurationMs-1
+		if minT < timeInterval.MinT {
+			minT = timeInterval.MinT
+		}
+		if maxT > timeInterval.MaxT {
+			maxT = timeInterval.MaxT
+		}
+
+		shard.DataStorageRLock()
+		chunkIterator := NewChunkIterator(shard.LSS().Raw(), LsIdBatchSize, shard.DataStorage().Raw(), minT, maxT)
+		shard.DataStorageRUnlock()
+
+		if writer, err := newBlockWriter(w.dataDir, w.maxBlockChunkSegmentSize, NewIndexWriter(shard.LSS().Raw()), chunkIterator); err == nil {
+			writers.append(writer)
+		} else {
+			writers.close()
+			return blockWriters{}, err
+		}
+	}
+
+	return writers, nil
+}
+
+func (w *Writer) recodeAndWriteChunks(shard relabeler.Shard, writers blockWriters) error {
+	shard.DataStorageRLock()
+	loader := shard.DataStorage().CreateRevertableLoader(shard.LSS().Raw(), LsIdBatchSize)
+	shard.DataStorageRUnlock()
+
+	isFirstBatch := true
+
+	loadData := func() (bool, error) {
+		if isFirstBatch {
+			isFirstBatch = false
+		} else {
+			if !loader.NextBatch() {
+				return false, nil
+			}
+		}
+
+		if shard.UnloadedDataStorage() == nil {
+			return true, nil
+		}
+
+		return true, shard.UnloadedDataStorage().ForEachSnapshot(loader.Load)
+	}
+
+	for {
+		shard.DataStorageLock()
+		hasMoreData, err := loadData()
+		shard.DataStorageUnlock()
+
+		if !hasMoreData {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		shard.DataStorageRLock()
+		err = writers.recodeAndWriteChunksBatch()
+		shard.DataStorageRUnlock()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return writers.writeRestOfRecodedChunks()
+}
+
+func closeAll(closers ...io.Closer) error {
+	var errs error
+	for _, closer := range closers {
+		errs = errors.Join(errs, closer.Close())
+	}
+	return errs
+}
+
+>>>>>>>> 820ec1690 (Head keeper (#154)):pp/go/relabeler/block/writer.go
 func adjustBlockMetaTimeRange(blockMeta *tsdb.BlockMeta, mint, maxt int64) {
 	if mint < blockMeta.MinTime {
 		blockMeta.MinTime = mint
