003925180f84902f7d3e991417d0dd827de84e99
Alexandr Yudin <alexandr.yudin@flant.com>
2025-09-30 16:26:47 +0000

    for save

--- pp/go/storage/types.go
+++ pp/go/storage/types.go
@@ -3,7 +3,6 @@ package storage
 import (
 	"github.com/prometheus/prometheus/pp/go/cppbridge"
 	"github.com/prometheus/prometheus/pp/go/storage/head/head"
-	"github.com/prometheus/prometheus/pp/go/storage/head/proxy"
 	"github.com/prometheus/prometheus/pp/go/storage/head/shard"
 	"github.com/prometheus/prometheus/pp/go/storage/head/shard/wal"
 	"github.com/prometheus/prometheus/pp/go/storage/head/shard/wal/writer"
@@ -24,5 +23,6 @@ type PerGoroutineShard = shard.PerGoroutineShard
 // HeadOnDisk [head.Head] with [ShardOnDisk].
 type HeadOnDisk = head.Head[*ShardOnDisk, *PerGoroutineShard]
 
-// ProxyHead [proxy.Proxy] for [HeadOnDisk]s.
-type ProxyHead = proxy.Proxy[*HeadOnDisk]
+// TODO DELETE
+// // ProxyHead [proxy.Proxy] for [HeadOnDisk]s.
+// type ProxyHead = proxy.Proxy[*HeadOnDisk]
