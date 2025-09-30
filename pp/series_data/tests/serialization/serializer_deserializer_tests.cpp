1da3536e927af91bdf9968a31cc58a6aef097db3
Gleb Shigin <gleb.shigin@gmail.com>
2025-09-30 17:03:34 +0300

    new model initial

--- pp/series_data/tests/serialization/serializer_deserializer_tests.cpp
+++ pp/series_data/tests/serialization/serializer_deserializer_tests.cpp
@@ -20,7 +20,7 @@ using series_data::encoder::SampleList;
 using series_data::querier::QueriedChunk;
 using series_data::querier::QueriedChunkList;
 using series_data::serialization::Deserializer;
-using series_data::serialization::Serializer;
+using series_data::serialization::old_::Serializer;
 
 class SerializerDeserializerTrait {
  protected:
