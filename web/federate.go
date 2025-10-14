6f39ebfd51efcc84cc8bc577e6f1a845e434e114
Alexandr Yudin <alexandr.yudin@flant.com>
2025-10-14 14:20:02 +0000

    fix

--- web/federate.go
+++ web/federate.go
@@ -300,11 +300,3 @@ Loop:
 		}
 	}
 }
-
-func (h *Handler) federationMock(w http.ResponseWriter, req *http.Request) {
-	h.mtx.RLock()
-	defer h.mtx.RUnlock()
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
-}
