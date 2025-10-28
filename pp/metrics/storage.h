d70ec23fb786de0e9d8c2a19d36938204d1a0743
Vladimir Pustovalov <cherep@sura.ru>
2025-10-28 10:30:06 +0300

    created metrics::Storage

--- pp/metrics/storage.h
+++ pp/metrics/storage.h
@@ -4,12 +4,81 @@
 
 namespace metrics {
 
-inline MetricsPageList storage;
+class Storage {
+ public:
+  class IteratorSentinel {};
+
+  class Iterator {
+   public:
+    struct Item {
+      const MetricsPageControlBlock* page{};
+      const Metric* metric{};
+
+      bool operator==(const Item&) const noexcept = default;
+    };
+
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = Item;
+    using difference_type = ptrdiff_t;
+    using pointer = value_type;
+    using reference = value_type;
+
+    explicit Iterator(const MetricsPageList& storage) : page_iterator_(storage.begin()), metric_iterator_(*page_iterator_) {
+      if (metric_iterator_ != MetricsPageControlBlock::end()) {
+        fill_item();
+      }
+    }
+
+    [[nodiscard]] PROMPP_ALWAYS_INLINE const value_type& operator*() const noexcept { return item_; }
+    [[nodiscard]] PROMPP_ALWAYS_INLINE const value_type* operator->() const noexcept { return &item_; }
+
+    PROMPP_ALWAYS_INLINE Iterator& operator++() noexcept {
+      if (++metric_iterator_ != MetricsPageControlBlock::end()) {
+        fill_item();
+      } else {
+        if (metric_iterator_ = MetricsPageControlBlock::Iterator(*++page_iterator_); metric_iterator_ != MetricsPageControlBlock::end()) {
+          fill_item();
+        }
+      }
+
+      return *this;
+    }
+
+    PROMPP_ALWAYS_INLINE Iterator operator++(int) noexcept {
+      const auto it = *this;
+      ++*this;
+      return it;
+    }
+
+    PROMPP_ALWAYS_INLINE bool operator==(const IteratorSentinel&) const noexcept { return page_iterator_ == MetricsPageList::end(); }
+
+   private:
+    MetricsPageList::Iterator page_iterator_;
+    MetricsPageControlBlock::Iterator metric_iterator_;
+    Item item_;
+
+    void fill_item() { item_ = {.page = *page_iterator_, .metric = *metric_iterator_}; }
+  };
+
+  PROMPP_ALWAYS_INLINE void add(MetricsPageControlBlock* page) { page_list_.add(page); }
+  PROMPP_ALWAYS_INLINE void remove_unused_pages() { page_list_.remove_unused_pages(); }
+
+  [[nodiscard]] PROMPP_ALWAYS_INLINE Iterator begin() const noexcept { return Iterator(page_list_); }
+  [[nodiscard]] PROMPP_ALWAYS_INLINE IteratorSentinel static end() noexcept { return {}; }
+
+ private:
+  MetricsPageList page_list_;
+};
+
+PROMPP_ALWAYS_INLINE Storage& storage() {
+  static Storage storage;
+  return storage;
+}
 
 template <class MetricsPageType, class LabelSet>
-PROMPP_ALWAYS_INLINE MetricsPageType* CreateMetricsPage(LabelSet&& label_set) {
+PROMPP_ALWAYS_INLINE MetricsPageType* CreateMetricsPage(LabelSet&& label_set, Storage& s = storage()) {
   auto* page = new MetricsPageType(std::forward<LabelSet>(label_set));
-  storage.add(page);
+  s.add(page);
   return page;
 }
 
---
+++ pp/metrics/storage_tests.cpp
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+
+#include "counter.h"
+#include "storage.h"
+
+namespace {
+
+using metrics::Counter;
+using metrics::CreateMetricsPage;
+using metrics::Metric;
+using metrics::MetricsPage;
+using metrics::MetricsPageControlBlock;
+using metrics::MetricsPageList;
+using metrics::Storage;
+using PromPP::Primitives::LabelViewSet;
+
+class MetricsStorageIteratorFixture : public testing::Test {
+ protected:
+  Storage storage_;
+
+  struct Metrics1 final : MetricsPage<Metrics1> {
+    using MetricsPage::MetricsPage;
+
+    Counter<> counter{"counter", 64};
+  };
+
+  struct Metrics2 final : MetricsPage<Metrics2> {
+    using MetricsPage::MetricsPage;
+
+    Counter<> counter1{"counter1", 64};
+    Counter<> counter2{"counter2", 64};
+  };
+};
+
+TEST_F(MetricsStorageIteratorFixture, EmptyStorage) {
+  // Arrange
+  std::vector<Storage::Iterator::Item> items;
+
+  // Act
+  std::ranges::copy(storage_, std::back_inserter(items));
+
+  // Assert
+  EXPECT_TRUE(items.empty());
+}
+
+TEST_F(MetricsStorageIteratorFixture, TwoPages) {
+  // Arrange
+  const auto page1 = CreateMetricsPage<Metrics1>(LabelViewSet{{"page", "1"}}, storage_);
+  const auto page2 = CreateMetricsPage<Metrics2>(LabelViewSet{{"page", "2"}}, storage_);
+
+  std::vector<Storage::Iterator::Item> items;
+
+  // Act
+  std::ranges::copy(storage_, std::back_inserter(items));
+
+  // Assert
+  EXPECT_TRUE(std::ranges::equal(
+      std::vector<Storage::Iterator::Item>{
+          {.page = page2, .metric = &page2->counter1},
+          {.page = page2, .metric = &page2->counter2},
+          {.page = page1, .metric = &page1->counter},
+      },
+      items));
+}
+
+}  // namespace
