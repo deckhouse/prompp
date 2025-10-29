dad57af49efc2573a2f74be98ea8ab50cdfb89f6
Vladimir Pustovalov <cherep@sura.ru>
2025-10-29 14:40:31 +0300

    added unit tests

--- pp/go/cppbridge/metrics_test.go
+++ pp/go/cppbridge/metrics_test.go
@@ -1,19 +1,18 @@
 package cppbridge
 
 import (
-	"runtime"
 	"testing"
 
 	dto "github.com/prometheus/client_model/go"
 	"github.com/stretchr/testify/suite"
 )
 
-type MetricsSuite struct {
+type CppMetricsSuite struct {
 	suite.Suite
 }
 
-func TestMetricsSuite(t *testing.T) {
-	suite.Run(t, new(MetricsSuite))
+func TestCppMetricsSuite(t *testing.T) {
+	suite.Run(t, new(CppMetricsSuite))
 }
 
 type cppMetric struct {
@@ -30,7 +29,7 @@ func newCppMetric(metric *dto.Metric) cppMetric {
 	return m
 }
 
-func (s *MetricsSuite) getMetrics() []cppMetric {
+func (s *CppMetricsSuite) getMetrics() []cppMetric {
 	metrics := []cppMetric(nil)
 	for metric := range CppMetrics {
 		metrics = append(metrics, newCppMetric(metric))
@@ -39,7 +38,23 @@ func (s *MetricsSuite) getMetrics() []cppMetric {
 	return metrics
 }
 
-func (s *MetricsSuite) TestEnumMetrics() {
+func (s *CppMetricsSuite) assertCounter(metric cppMetric, expectedLabels Labels, expectedValue float64) {
+	s.Require().NotNil(metric.counter)
+	s.Equal(expectedValue, *metric.counter.Value)
+	s.Equal(expectedLabels, metric.labels)
+}
+
+func (s *CppMetricsSuite) TestNoMetricPages() {
+	// Arrange
+
+	// Act
+	metrics := s.getMetrics()
+
+	// Assert
+	s.Len(metrics, 0)
+}
+
+func (s *CppMetricsSuite) TestOneMetricsPage() {
 	// Arrange
 	const counterValue = 123
 
@@ -51,12 +66,35 @@ func (s *MetricsSuite) TestEnumMetrics() {
 
 	// Assert
 	s.Require().Len(metrics, 1)
-	s.Require().NotNil(metrics[0].counter)
-	s.Equal(float64(counterValue), *metrics[0].counter.Value)
-	s.Equal(Labels{
+	s.assertCounter(metrics[0], Labels{
 		Label{Name: "__name__", Value: "counter"},
 		Label{Name: "metrics_page", Value: "for_test"},
-	}, metrics[0].labels)
+	}, float64(counterValue))
+}
 
-	runtime.KeepAlive(page)
+func (s *CppMetricsSuite) TestTwoMetricPages() {
+	// Arrange
+	const counterValue1 = 123
+	const counterValue2 = 321
+
+	page1 := prometheusMetricsPageForTestCtor(Labels{Label{Name: "metrics_page1", Value: "for_test"}}, "counter1", counterValue1)
+	page2 := prometheusMetricsPageForTestCtor(Labels{Label{Name: "metrics_page2", Value: "for_test"}}, "counter2", counterValue2)
+	defer func() {
+		prometheusMetricsPageForTestDetach(page1)
+		prometheusMetricsPageForTestDetach(page2)
+	}()
+
+	// Act
+	metrics := s.getMetrics()
+
+	// Assert
+	s.Require().Len(metrics, 2)
+	s.assertCounter(metrics[0], Labels{
+		Label{Name: "__name__", Value: "counter2"},
+		Label{Name: "metrics_page2", Value: "for_test"},
+	}, float64(counterValue2))
+	s.assertCounter(metrics[1], Labels{
+		Label{Name: "__name__", Value: "counter1"},
+		Label{Name: "metrics_page1", Value: "for_test"},
+	}, float64(counterValue1))
 }
