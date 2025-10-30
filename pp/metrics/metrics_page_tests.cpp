83af84e8d299c63ada71e9192c6f3a47c50eae8c
Vladimir Pustovalov <cherep@sura.ru>
2025-10-30 14:59:39 +0300

    fixed naming

--- pp/metrics/metrics_page_tests.cpp
+++ pp/metrics/metrics_page_tests.cpp
@@ -21,7 +21,7 @@ TEST_F(MetricsPageFixture, TestIterator) {
   struct Metrics : MetricsPage<Metrics> {
     using MetricsPage::MetricsPage;
 
-    Counter<uint16_t> uint64_counter{"uint16_counter", 16};
+    Counter<uint16_t> uint16_counter{"uint16_counter", 16};
     Counter<uint32_t> uint32_counter{"uint32_counter", 32};
     AtomicCounter<uint64_t> atomic_counter{"atomic_uint64_counter", 64};
   } const metrics(LabelViewSet{{"job", "test"}});
@@ -32,7 +32,7 @@ TEST_F(MetricsPageFixture, TestIterator) {
   std::ranges::copy(metrics, std::back_inserter(metric_pointers));
 
   // Assert
-  EXPECT_EQ((MetricsVector{&metrics.uint64_counter, &metrics.uint32_counter, &metrics.atomic_counter}), metric_pointers);
+  EXPECT_EQ((MetricsVector{&metrics.uint16_counter, &metrics.uint32_counter, &metrics.atomic_counter}), metric_pointers);
 }
 
 }  // namespace
