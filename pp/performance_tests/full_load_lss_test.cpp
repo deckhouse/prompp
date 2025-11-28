1030bfab67c335c0846174426cc40a7e3ae3027e
cherep58 <cherep@sura.ru>
2025-11-28 12:06:58 +0300

    Removed unused decoding tables (#207)
    * removed ParallelEncodingBimap
    
    * removed OrderedIndexingTable
    
    * removed OrderedEncodingBimap

--- pp/performance_tests/full_load_lss_test.cpp
+++ pp/performance_tests/full_load_lss_test.cpp
@@ -26,24 +26,6 @@ void full_load_lss::execute(const Config& config, Metrics& metrics) const {
                          << (std::chrono::duration_cast<std::chrono::microseconds>(now - start).count()));
   }
 
-  // test load time: ParallelEncodingBimap
-  {
-    std::ifstream infile(input_file_full_name(config), std::ios_base::binary);
-    BareBones::LZ4Stream::istream in(&infile);
-    if (!infile.is_open()) {
-      throw std::runtime_error("failed to open file '" + input_file_full_name(config) + "'");
-    }
-
-    Primitives::SnugComposites::LabelSet::ParallelEncodingBimap<BareBones::Vector> lss;
-
-    auto start = std::chrono::steady_clock::now();
-    in >> lss;
-    auto now = std::chrono::steady_clock::now();
-
-    metrics << (Metric() << "parallel_encoding_bimap_load_from_file_duration_microseconds"
-                         << (std::chrono::duration_cast<std::chrono::microseconds>(now - start).count()));
-  }
-
   // test load time: EncodingBimap
   {
     std::ifstream infile(input_file_full_name(config), std::ios_base::binary);
@@ -59,42 +41,6 @@ void full_load_lss::execute(const Config& config, Metrics& metrics) const {
     auto now = std::chrono::steady_clock::now();
 
     metrics << (Metric() << "encoding_bimap_load_from_file_duration_microseconds"
-                         << (std::chrono::duration_cast<std::chrono::microseconds>(now - start).count()));
-  }
-
-  // test load time: OrderedEncodingBimap
-  {
-    std::ifstream infile(input_file_full_name(config), std::ios_base::binary);
-    if (!infile.is_open()) {
-      throw std::runtime_error("failed to open file '" + input_file_full_name(config) + "'");
-    }
-
-    Primitives::SnugComposites::LabelSet::OrderedEncodingBimap<BareBones::Vector> lss;
-
-    BareBones::LZ4Stream::istream in(&infile);
-    auto start = std::chrono::steady_clock::now();
-    in >> lss;
-    auto now = std::chrono::steady_clock::now();
-
-    metrics << (Metric() << "ordered_encoding_bimap_load_from_file_duration_microseconds"
-                         << (std::chrono::duration_cast<std::chrono::microseconds>(now - start).count()));
-  }
-
-  // test load time: OrderedIndexingTable
-  {
-    std::ifstream infile(input_file_full_name(config), std::ios_base::binary);
-    if (!infile.is_open()) {
-      throw std::runtime_error("failed to open file '" + input_file_full_name(config) + "'");
-    }
-
-    Primitives::SnugComposites::LabelSet::OrderedIndexingTable<BareBones::Vector> lss;
-
-    BareBones::LZ4Stream::istream in(&infile);
-    auto start = std::chrono::steady_clock::now();
-    in >> lss;
-    auto now = std::chrono::steady_clock::now();
-
-    metrics << (Metric() << "ordered_indexing_table_load_from_file_duration_microseconds"
                          << (std::chrono::duration_cast<std::chrono::microseconds>(now - start).count()));
   }
 }
