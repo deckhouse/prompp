0f51f7dc7a3afcc26311810d7f4271a58f22e62c
Vladimir Pustovalov <cherep@sura.ru>
2026-02-06 14:11:56 +0300

    added idelta promql function handling

--- pp/go/storage/querier/series_test.go
+++ pp/go/storage/querier/series_test.go
@@ -876,3 +876,42 @@ func (s *SeriesSetTestSuite) TestIRateFunc() {
 		},
 	}, storagetest.TimeSeriesFromSeriesSet(seriesSet, true))
 }
+
+func (s *SeriesSetTestSuite) TestIDeltaFunc() {
+	// Arrange
+	storagetest.MustAppendTimeSeriesToLSSAndDataStorage(s.lss, s.ds, []storagetest.TimeSeries{
+		{
+			Labels: labels.FromStrings("__name__", "metric", "job", "test"),
+			Samples: []cppbridge.Sample{
+				{Timestamp: 100, Value: 1.0},
+				{Timestamp: 150, Value: 2.0},
+				{Timestamp: 200, Value: 3.0},
+				{Timestamp: 250, Value: math.Float64frombits(value.StaleNaN)},
+				{Timestamp: 300, Value: 4.0},
+			},
+		},
+	}...)
+	hints := &storage.SelectHints{
+		Start: 100,
+		End:   300,
+		Func:  "idelta",
+	}
+
+	// Act
+	seriesSet := s.query(s.lss, s.ds, 0, 400, cppbridge.NoDownsampling, hints, model.LabelMatcher{
+		Name:        "__name__",
+		Value:       "metric",
+		MatcherType: model.MatcherTypeExactMatch,
+	})
+
+	// Assert
+	require.Equal(s.T(), []storagetest.TimeSeries{
+		{
+			Labels: labels.FromStrings("__name__", "metric", "job", "test"),
+			Samples: []cppbridge.Sample{
+				{Timestamp: 200, Value: 3.0},
+				{Timestamp: 300, Value: 4.0},
+			},
+		},
+	}, storagetest.TimeSeriesFromSeriesSet(seriesSet, true))
+}
