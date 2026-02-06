c4ad46dc7aa5f6d0caf82765395acbbb8e7baafc
Vladimir Pustovalov <cherep@sura.ru>
2026-02-06 16:05:53 +0300

    created ResetsIterator

---
+++ pp/series_data/decoder/decorator/resets_iterator.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "primitives/primitives.h"
+#include "series_data/decoder/universal_decode_iterator.h"
+
+namespace series_data::decoder::decorator {
+
+class ResetsIterator {
+ public:
+  DECODE_ITERATOR_TYPE_TRAITS();
+
+  explicit ResetsIterator(PromPP::Primitives::TimeInterval interval) : interval_(interval) {}
+  explicit ResetsIterator(UniversalDecodeIterator&& iterator, PromPP::Primitives::TimeInterval interval) : iterator_(iterator), interval_(interval) {
+    seek_to_first_sample();
+  }
+
+  PROMPP_ALWAYS_INLINE ResetsIterator& operator=(UniversalDecodeIterator&& iterator) {
+    iterator_ = std::move(iterator);
+    seek_to_first_sample();
+    return *this;
+  }
+
+  PROMPP_ALWAYS_INLINE const encoder::Sample& operator*() const { return iterator_.operator*(); }
+  PROMPP_ALWAYS_INLINE const encoder::Sample* operator->() const { return iterator_.operator->(); }
+
+  PROMPP_ALWAYS_INLINE bool operator==(const DecodeIteratorSentinel&) const { return iterator_->timestamp == kInvalidTimestamp; }
+
+  PROMPP_ALWAYS_INLINE ResetsIterator& operator++() {
+    seek_to_next_sample();
+    return *this;
+  }
+
+  PROMPP_ALWAYS_INLINE ResetsIterator operator++(int) {
+    const auto result = *this;
+    ++*this;
+    return result;
+  }
+
+ protected:
+  UniversalDecodeIterator iterator_;
+  PromPP::Primitives::TimeInterval interval_;
+
+  void seek_to_first_sample() {
+    bool has_value{};
+
+    iterator_.seek([&has_value, this](PromPP::Primitives::Timestamp timestamp, double value) {
+      if (timestamp < interval_.min) {
+        return SeekResult::kNext;
+      }
+      if (timestamp > interval_.max) {
+        return SeekResult::kStop;
+      }
+
+      if (BareBones::Encoding::Gorilla::isstalenan(value)) [[unlikely]] {
+        return SeekResult::kNext;
+      }
+
+      has_value = true;
+      return SeekResult::kUpdateSampleNextAndStop;
+    });
+
+    if (!has_value) [[unlikely]] {
+      iterator_.invalidate();
+    }
+  }
+
+  PROMPP_ALWAYS_INLINE void seek_to_next_sample() {
+    bool has_resets{};
+
+    iterator_.seek([&has_resets, this](PromPP::Primitives::Timestamp timestamp, double value) {
+      if (timestamp > interval_.max) {
+        return SeekResult::kStop;
+      }
+
+      if (BareBones::Encoding::Gorilla::isstalenan(value)) [[unlikely]] {
+        return SeekResult::kNext;
+      }
+
+      if (value < iterator_->value) [[unlikely]] {
+        has_resets = true;
+        return SeekResult::kUpdateSampleNextAndStop;
+      }
+
+      return SeekResult::kUpdateSample;
+    });
+
+    if (!has_resets) [[unlikely]] {
+      iterator_.invalidate();
+    }
+  }
+};
+
+}  // namespace series_data::decoder::decorator
---
+++ pp/series_data/tests/decoder/decorator/resets_iterator_tests.cpp
@@ -0,0 +1,112 @@
+#include <gtest/gtest.h>
+
+#include "series_data/data_storage.h"
+#include "series_data/decoder.h"
+#include "series_data/decoder/decorator/resets_iterator.h"
+#include "series_data/encoder.h"
+
+namespace {
+
+using BareBones::Encoding::Gorilla::STALE_NAN;
+using series_data::DataStorage;
+using series_data::Decoder;
+using series_data::Encoder;
+using series_data::chunk::DataChunk;
+using series_data::decoder::DecodeIteratorSentinel;
+using series_data::decoder::UniversalDecodeIterator;
+using series_data::decoder::decorator::ResetsIterator;
+using series_data::encoder::Sample;
+
+struct ResetsIteratorCase {
+  std::vector<Sample> samples;
+  PromPP::Primitives::TimeInterval interval;
+  std::vector<Sample> expected{};
+};
+
+class ResetsIteratorFixture : public ::testing::TestWithParam<ResetsIteratorCase> {
+ protected:
+  DataStorage storage_;
+
+  void SetUp() override {
+    Encoder encoder(storage_);
+    for (const auto& sample : GetParam().samples) {
+      encoder.encode(0, sample.timestamp, sample.value);
+    }
+  }
+};
+
+TEST_P(ResetsIteratorFixture, Test) {
+  // Arrange
+  std::vector<Sample> actual_samples;
+
+  // Act
+  Decoder::create_decode_iterator<DataChunk::Type::kOpen>(storage_, storage_.open_chunks[0], [&actual_samples]<typename Iterator>(Iterator&& begin, auto&&) {
+    std::ranges::copy(ResetsIterator(UniversalDecodeIterator{std::in_place_type<Iterator>, std::forward<Iterator>(begin)}, GetParam().interval),
+                      DecodeIteratorSentinel{}, std::back_inserter(actual_samples));
+  });
+
+  // Assert
+  EXPECT_EQ(GetParam().expected, actual_samples);
+}
+
+TEST_P(ResetsIteratorFixture, TestReset) {
+  // Arrange
+  std::vector<Sample> actual_samples;
+
+  // Act
+  Decoder::create_decode_iterator<DataChunk::Type::kOpen>(storage_, storage_.open_chunks[0], [&actual_samples]<typename Iterator>(Iterator&& begin, auto&&) {
+    ResetsIterator iterator(UniversalDecodeIterator{std::in_place_type<Iterator>, std::forward<Iterator>(begin)}, GetParam().interval);
+    std::advance(iterator, GetParam().samples.size());
+
+    iterator = UniversalDecodeIterator{std::in_place_type<Iterator>, std::forward<Iterator>(begin)};
+    std::ranges::copy(iterator, DecodeIteratorSentinel{}, std::back_inserter(actual_samples));
+  });
+
+  // Assert
+  EXPECT_EQ(GetParam().expected, actual_samples);
+}
+
+INSTANTIATE_TEST_SUITE_P(OneSample,
+                         ResetsIteratorFixture,
+                         testing::Values(ResetsIteratorCase{.samples{Sample{.timestamp = 100, .value = 1.0}},
+                                                            .interval{.min = 0, .max = 100},
+                                                            .expected{Sample{.timestamp = 100, .value = 1.0}}},
+                                         ResetsIteratorCase{.samples{Sample{.timestamp = 100, .value = 1.0}}, .interval{.min = 0, .max = 99}, .expected{}},
+                                         ResetsIteratorCase{.samples{Sample{.timestamp = 100, .value = 1.0}}, .interval{.min = 101, .max = 200}, .expected{}}));
+
+INSTANTIATE_TEST_SUITE_P(
+    StaleNan,
+    ResetsIteratorFixture,
+    testing::Values(ResetsIteratorCase{.samples{
+                                           Sample{.timestamp = 5, .value = STALE_NAN},
+                                           Sample{.timestamp = 10, .value = 1.0},
+                                           Sample{.timestamp = 20, .value = STALE_NAN},
+                                           Sample{.timestamp = 30, .value = 1.0},
+                                       },
+                                       .interval{.min = 0, .max = 100},
+                                       .expected{Sample{.timestamp = 10, .value = 1.0}}},
+                    ResetsIteratorCase{.samples{Sample{.timestamp = 100, .value = STALE_NAN}}, .interval{.min = 0, .max = 100}, .expected{}}));
+
+INSTANTIATE_TEST_SUITE_P(TimeInterval,
+                         ResetsIteratorFixture,
+                         testing::Values(ResetsIteratorCase{.samples{
+                                                                Sample{.timestamp = 100, .value = 4.0},
+                                                                Sample{.timestamp = 150, .value = 3.0},
+                                                                Sample{.timestamp = 180, .value = 2.0},
+                                                                Sample{.timestamp = 200, .value = 1.0},
+                                                            },
+                                                            .interval{.min = 100, .max = 200},
+                                                            .expected{Sample{.timestamp = 100, .value = 4.0}, Sample{.timestamp = 150, .value = 3.0},
+                                                                      Sample{.timestamp = 180, .value = 2.0}, Sample{.timestamp = 200, .value = 1.0}}},
+                                         ResetsIteratorCase{.samples{
+                                                                Sample{.timestamp = 100, .value = 3.0},
+                                                                Sample{.timestamp = 120, .value = 0.0},
+                                                                Sample{.timestamp = 150, .value = 0.0},
+                                                                Sample{.timestamp = 180, .value = 1.0},
+                                                                Sample{.timestamp = 200, .value = 0.0},
+                                                            },
+                                                            .interval{.min = 100, .max = 200},
+                                                            .expected{Sample{.timestamp = 100, .value = 3.0}, Sample{.timestamp = 120, .value = 0.0},
+                                                                      Sample{.timestamp = 200, .value = 0.0}}}));
+
+}  // namespace
