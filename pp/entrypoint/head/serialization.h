f4deb6d03224f56933234b2d3ba5a64390566427
Vladimir Pustovalov <cherep@sura.ru>
2026-02-06 18:23:11 +0300

    created create_decode_iterator method

--- pp/entrypoint/head/serialization.h
+++ pp/entrypoint/head/serialization.h
@@ -25,57 +25,7 @@ class SerializedDataGo {
 
   [[nodiscard]] PROMPP_ALWAYS_INLINE auto next() noexcept { return data_view_.next_series(); }
   [[nodiscard]] PROMPP_ALWAYS_INLINE SerializedDataIterator iterator(uint32_t chunk_id) const noexcept {
-    if (downsampling_ms_ != ::series_data::decoder::decorator::kNoDownsampling) [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::DownsamplingIterator>, downsampling_ms_));
-    }
-
-    if (select_hints_.func == "rate" || select_hints_.func == "increase") [[likely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::RateIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "irate" || select_hints_.func == "idelta") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::IRateIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "min_over_time") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::MinOverTimeIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "max_over_time") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::MaxOverTimeIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "last_over_time") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(
-          chunk_id, DecodeIterator(std::in_place_type<DecodeIterator::LastOverTimeIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "sum_over_time") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::SumOverTimeIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "changes") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::ChangesIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "delta") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::DeltaIterator>, select_hints_.interval));
-    }
-
-    if (select_hints_.func == "resets") [[unlikely]] {
-      return data_view_.create_series_iterator<DecodeIterator>(chunk_id,
-                                                               DecodeIterator(std::in_place_type<DecodeIterator::ResetsIterator>, select_hints_.interval));
-    }
-
-    return data_view_.create_series_iterator<DecodeIterator>(chunk_id, DecodeIterator(std::in_place_type<DecodeIterator::UniversalDecodeIterator>));
+    return data_view_.create_series_iterator<DecodeIterator>(chunk_id, create_decode_iterator(select_hints_, downsampling_ms_));
   }
 
  private:
