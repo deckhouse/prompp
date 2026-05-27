88c491269be5c30a38cfbb90d614080f4ad1daff
Vladimir Pustovalov <cherep@sura.ru>
2026-05-27 13:26:58 +0300

    created LookbackDeltaIterator

---
+++ pp/series_data/decoder/decorator/lookback_delta_iterator.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "primitives/primitives.h"
+#include "series_data/decoder/universal_decode_iterator.h"
+
+namespace series_data::decoder::decorator {
+
+template <class Iterator = UniversalDecodeIterator>
+class LookbackDeltaIterator {
+ public:
+  DECODE_ITERATOR_TYPE_TRAITS();
+
+  LookbackDeltaIterator(Iterator&& iterator, PromPP::Primitives::Timestamp lookback_delta) : iterator_(std::move(iterator)), lookback_delta_(lookback_delta) {
+    update_sample();
+  }
+
+  [[nodiscard]] PROMPP_ALWAYS_INLINE const PromPP::Primitives::TimeInterval& interval() const noexcept { return iterator_.interval(); }
+  PROMPP_ALWAYS_INLINE void set_interval(const PromPP::Primitives::TimeInterval& interval) {
+    iterator_.set_interval(interval);
+    update_sample();
+  }
+
+  PROMPP_ALWAYS_INLINE const encoder::Sample& operator*() const { return sample_; }
+  PROMPP_ALWAYS_INLINE const encoder::Sample* operator->() const { return &sample_; }
+
+  PROMPP_ALWAYS_INLINE bool operator==(const DecodeIteratorSentinel&) const { return sample_.timestamp == kInvalidTimestamp; }
+
+  PROMPP_ALWAYS_INLINE LookbackDeltaIterator& operator++() {
+    ++iterator_;
+    update_sample();
+    return *this;
+  }
+
+  PROMPP_ALWAYS_INLINE LookbackDeltaIterator operator++(int) {
+    const auto result = *this;
+    ++*this;
+    return result;
+  }
+
+ protected:
+  static constexpr auto kInvalidSample = encoder::Sample{.timestamp = kInvalidTimestamp, .value = BareBones::Encoding::Gorilla::STALE_NAN};
+
+  encoder::Sample sample_{kInvalidSample};
+  Iterator iterator_;
+  PromPP::Primitives::Timestamp lookback_delta_;
+
+  PROMPP_ALWAYS_INLINE void update_sample() noexcept {
+    if (iterator_ != DecodeIteratorSentinel{}) [[likely]] {
+      sample_ = *iterator_;
+    } else if (!sample_in_lookback_delta_interval()) {
+      sample_ = kInvalidSample;
+    }
+  }
+
+  [[nodiscard]] PROMPP_ALWAYS_INLINE bool sample_in_lookback_delta_interval() const noexcept {
+    return sample_.timestamp > iterator_.interval().max - lookback_delta_;
+  }
+};
+
+}  // namespace series_data::decoder::decorator
---
+++ pp/series_data/tests/decoder/decorator/lookback_delta_iterator_tests.cpp
@@ -0,0 +1,149 @@
+#include <gtest/gtest.h>
+
+#include "series_data/data_storage.h"
+#include "series_data/decoder.h"
+#include "series_data/decoder/decorator/last_over_time.h"
+#include "series_data/decoder/decorator/lookback_delta_iterator.h"
+#include "series_data/encoder.h"
+
+namespace {
+
+using BareBones::Encoding::Gorilla::STALE_NAN;
+using PromPP::Primitives::TimeInterval;
+using PromPP::Primitives::Timestamp;
+using series_data::DataStorage;
+using series_data::Decoder;
+using series_data::Encoder;
+using series_data::chunk::DataChunk;
+using series_data::decoder::DecodeIteratorSentinel;
+using series_data::decoder::kInvalidTimestamp;
+using series_data::decoder::UniversalDecodeIterator;
+using series_data::decoder::decorator::LastOverTimeIterator;
+using series_data::decoder::decorator::LookbackDeltaIterator;
+using series_data::encoder::Sample;
+
+constexpr Sample kInvalidSample{.timestamp = kInvalidTimestamp, .value = STALE_NAN};
+
+struct LookbackDeltaIteratorCase {
+  std::vector<Sample> samples;
+  std::vector<TimeInterval> intervals;
+  Timestamp lookback_delta{};
+  std::vector<Sample> expected{};
+};
+
+class LookbackDeltaIteratorFixture : public testing::TestWithParam<LookbackDeltaIteratorCase> {
+ protected:
+  using Iterator = LookbackDeltaIterator<LastOverTimeIterator<>>;
+
+  DataStorage storage_;
+  Encoder<> encoder_{storage_};
+
+  void SetUp() override {
+    for (const auto& sample : GetParam().samples) {
+      encoder_.encode(0, sample.timestamp, sample.value);
+    }
+  }
+
+  std::vector<Sample> get_samples(const std::vector<TimeInterval>& intervals, Timestamp lookback_delta) {
+    std::vector<Sample> samples;
+
+    Decoder::create_decode_iterator<DataChunk::Type::kOpen>(
+        storage_, storage_.open_chunks[0], [&samples, &intervals, lookback_delta]<typename DecodeIterator>(DecodeIterator&& begin, auto&&) {
+          Iterator it(LastOverTimeIterator(UniversalDecodeIterator{std::in_place_type<DecodeIterator>, std::forward<DecodeIterator>(begin)}, intervals.front()),
+                      lookback_delta);
+          samples.emplace_back(*it);
+
+          std::ranges::for_each(intervals.begin() + 1, intervals.end(), [&samples, &it](const auto& interval) {
+            it.set_interval(interval);
+            samples.emplace_back(*it);
+          });
+        });
+
+    return samples;
+  }
+};
+
+TEST_P(LookbackDeltaIteratorFixture, Test) {
+  // Arrange
+
+  // Act
+  const auto actual_samples = get_samples(GetParam().intervals, GetParam().lookback_delta);
+
+  // Assert
+  EXPECT_EQ(GetParam().expected, actual_samples);
+}
+
+INSTANTIATE_TEST_SUITE_P(OneSample,
+                         LookbackDeltaIteratorFixture,
+                         testing::Values(LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 100, .value = 1.0}},
+                                                                   .intervals{{.min = 0, .max = 100}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{Sample{.timestamp = 100, .value = 1.0}}},
+                                         LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 100, .value = 1.0}},
+                                                                   .intervals{{.min = 0, .max = 99}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{kInvalidSample}},
+                                         LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 100, .value = 1.0}},
+                                                                   .intervals{{.min = 101, .max = 200}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{kInvalidSample}}));
+
+INSTANTIATE_TEST_SUITE_P(StaleNan,
+                         LookbackDeltaIteratorFixture,
+                         testing::Values(LookbackDeltaIteratorCase{.samples{
+                                                                       Sample{.timestamp = 5, .value = STALE_NAN},
+                                                                       Sample{.timestamp = 10, .value = 1.0},
+                                                                       Sample{.timestamp = 20, .value = STALE_NAN},
+                                                                       Sample{.timestamp = 30, .value = 1.1},
+                                                                   },
+                                                                   .intervals{{.min = 0, .max = 100}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{Sample{.timestamp = 30, .value = 1.1}}},
+                                         LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 100, .value = STALE_NAN}},
+                                                                   .intervals{{.min = 0, .max = 100}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{kInvalidSample}}));
+
+INSTANTIATE_TEST_SUITE_P(TimeInterval,
+                         LookbackDeltaIteratorFixture,
+                         testing::Values(LookbackDeltaIteratorCase{.samples{
+                                                                       Sample{.timestamp = 99, .value = 1.1},
+                                                                       Sample{.timestamp = 100, .value = 1.0},
+                                                                       Sample{.timestamp = 200, .value = 1.0},
+                                                                       Sample{.timestamp = 201, .value = 1.1},
+                                                                   },
+                                                                   .intervals{{.min = 100, .max = 200}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{Sample{.timestamp = 200, .value = 1.0}}},
+                                         LookbackDeltaIteratorCase{.samples{
+                                                                       Sample{.timestamp = 100, .value = 1.0},
+                                                                       Sample{.timestamp = 150, .value = 1.1},
+                                                                       Sample{.timestamp = 200, .value = 1.2},
+                                                                   },
+                                                                   .intervals{{.min = 100, .max = 200}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{Sample{.timestamp = 200, .value = 1.2}}}));
+
+INSTANTIATE_TEST_SUITE_P(LookbackDeltaBoundary,
+                         LookbackDeltaIteratorFixture,
+                         testing::Values(LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 50, .value = 1.0}},
+                                                                   .intervals{{.min = 100, .max = 200}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{kInvalidSample}},
+                                         LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 51, .value = 1.0}},
+                                                                   .intervals{{.min = 100, .max = 200}},
+                                                                   .lookback_delta = 50,
+                                                                   .expected{kInvalidSample}}));
+
+INSTANTIATE_TEST_SUITE_P(KeepsSampleAfterIntervalShift,
+                         LookbackDeltaIteratorFixture,
+                         testing::Values(LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 150, .value = 2.0}},
+                                                                   .intervals{{.min = 100, .max = 200}, {.min = 201, .max = 300}},
+                                                                   .lookback_delta = 151,
+                                                                   .expected{Sample{.timestamp = 150, .value = 2.0}, Sample{.timestamp = 150, .value = 2.0}}},
+                                         LookbackDeltaIteratorCase{.samples{Sample{.timestamp = 150, .value = 2.0}},
+                                                                   .intervals{{.min = 100, .max = 200}, {.min = 201, .max = 300}},
+                                                                   .lookback_delta = 150,
+                                                                   .expected{Sample{.timestamp = 150, .value = 2.0}, kInvalidSample}}));
+
+}  // namespace
