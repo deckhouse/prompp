f32f3cff1455e79e1ccfc26e654ee0043e3bb023
Alexandr Yudin <57181751+u-veles-a@users.noreply.github.com>
2026-05-30 11:24:57 +0000

    WIP: tuning asan test
    Signed-off-by: Alexandr Yudin <57181751+u-veles-a@users.noreply.github.com>

--- pp/go/storage/querier/querier_optimize_variables_asan_test.go
+++ pp/go/storage/querier/querier_optimize_variables_asan_test.go
@@ -15,15 +15,13 @@ const (
 
 // defaultSteps is the default steps.
 var defaultSteps = []time.Duration{
-	defaultStep - time.Second,       // [14s]
-	(defaultStep - time.Second) * 2, // [29s]
-	defaultStep * 2,                 // [30s]
+	defaultStep - time.Second, // [14s]
+	defaultStep * 2,           // [30s]
 }
 
 // defaultSubQueries is the default subqueries.
 var defaultSubQueries = []subQuery{
 	{window: model.Duration(defaultStep), step: 0},                 // [15s]
-	{window: model.Duration(defaultStep * 4), step: 0},             // [60s]
 	{window: model.Duration(defaultStep*4 + time.Second), step: 0}, // [61s]
 }
 
@@ -31,12 +29,10 @@ var defaultSubQueries = []subQuery{
 var defaultModifiers = []modifier{
 	modifierNone,
 	modifierEnd,
-	modifierStart,
 }
 
 // defaultOffsets is the default offsets.
 var defaultOffsets = []offset{
 	newOffset(0),
 	newOffset(defaultStep * defaultCountOfSteps / 2),
-	newOffset(-defaultStep * defaultCountOfSteps / 2),
 }
