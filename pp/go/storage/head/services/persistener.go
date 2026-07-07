b5f8a2857a972dc71af27023c3a89fee3b9fde48
Alexandr Yudin <57181751+u-veles-a@users.noreply.github.com>
2026-07-07 07:16:37 +0000

    add defaultTsdbRetentionPeriod
    Signed-off-by: Alexandr Yudin <57181751+u-veles-a@users.noreply.github.com>

--- pp/go/storage/head/services/persistener.go
+++ pp/go/storage/head/services/persistener.go
@@ -13,9 +13,15 @@ import (
 	"github.com/prometheus/prometheus/pp/go/util"
 )
 
-// defaultCoolingInterval the interval after which the rotation should have
-// taken place to eliminate errors in the selection from the catalog.
-const defaultCoolingInterval = 60 * time.Second
+const (
+	// defaultCoolingInterval the interval after which the rotation should have
+	// taken place to eliminate errors in the selection from the catalog.
+	defaultCoolingInterval = 60 * time.Second
+
+	// defaultTsdbRetentionPeriod is the default retention period for tsdb.
+	// Used only when tsdbRetentionPeriod is zero.
+	defaultTsdbRetentionPeriod = 15 * 24 * time.Hour
+)
 
 //
 // Persistener
@@ -217,6 +223,10 @@ func NewPersistenerService[
 	retentionPeriod time.Duration,
 	registerer prometheus.Registerer,
 ) *PersistenerService[TTask, TShard, TGoShard, THeadBlockWriter, THead, TProxyHead, TLoader] {
+	if tsdbRetentionPeriod == 0 {
+		tsdbRetentionPeriod = defaultTsdbRetentionPeriod
+	}
+
 	return &PersistenerService[TTask, TShard, TGoShard, THeadBlockWriter, THead, TProxyHead, TLoader]{
 		persistener: NewPersistener[TTask, TShard, TGoShard, THeadBlockWriter, THead](
 			hcatalog,
