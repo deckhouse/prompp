1f53ade11123068d75286c0129448a4794c460a3
Alexandr Yudin <alexandr.yudin@flant.com>
2026-08-27 09:32:24 +0000

    fix after merge

--- pp/go/cppbridge/entrypoint.h
+++ pp/go/cppbridge/entrypoint.h
@@ -117,22 +117,6 @@ extern "C" {
 #endif
 
 /**
- * @brief Initialize C++ feature flags
- *
- * @param args {
- *     features PromppFeatures
- * }
- */
-void prompp_feature_flags_initialize(void* args);
-
-#ifdef __cplusplus
-}
-#endif
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/**
  * @brief Return head status
  *
  * @param args {
