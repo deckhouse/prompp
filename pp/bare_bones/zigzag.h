a3fe6593ee49410c27c3384936cc0008f4d008ae
Vladimir Pustovalov <cherep@sura.ru>
2026-09-03 18:10:54 +0300

    replace inline __attribute__((always_inline)) with PROMPP_ALWAYS_INLINE

--- pp/bare_bones/zigzag.h
+++ pp/bare_bones/zigzag.h
@@ -6,21 +6,21 @@ namespace BareBones {
 namespace Encoding {
 
 namespace ZigZag {
-inline __attribute__((always_inline)) uint64_t encode(int64_t val) noexcept {
+PROMPP_ALWAYS_INLINE uint64_t encode(int64_t val) noexcept {
   // cppcheck-suppress shiftTooManyBitsSigned
   return (static_cast<uint64_t>(val) + static_cast<uint64_t>(val)) ^ (val >> 63);
 }
 
-inline __attribute__((always_inline)) uint32_t encode(int32_t val) noexcept {
+PROMPP_ALWAYS_INLINE uint32_t encode(int32_t val) noexcept {
   // cppcheck-suppress shiftTooManyBitsSigned
   return (static_cast<uint32_t>(val) + static_cast<uint32_t>(val)) ^ (val >> 31);
 }
 
-inline __attribute__((always_inline)) int64_t decode(uint64_t val) noexcept {
+PROMPP_ALWAYS_INLINE int64_t decode(uint64_t val) noexcept {
   return (val >> 1) ^ (0 - (val & 1));
 }
 
-inline __attribute__((always_inline)) int32_t decode(uint32_t val) noexcept {
+PROMPP_ALWAYS_INLINE int32_t decode(uint32_t val) noexcept {
   return (val >> 1) ^ (0 - (val & 1));
 }
 }  // namespace ZigZag
