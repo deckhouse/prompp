70da34daaaaebed38dd98acfd064057af0af5214
cherep58 <cherep@sura.ru>
2026-09-15 17:05:37 +0300

    replace inline __attribute__((always_inline)) with PROMPP_ALWAYS_INLINE (#496)
    * replace inline __attribute__((always_inline)) with PROMPP_ALWAYS_INLINE
    
    * fixed clang-tidy warnings
    
    (cherry picked from commit d3b16ec55f1d6f2bda9785797b99a8689b509477)

--- pp/bare_bones/zigzag.h
+++ pp/bare_bones/zigzag.h
@@ -2,28 +2,26 @@
 
 #include <cstdint>
 
-namespace BareBones {
-namespace Encoding {
+#include "preprocess.h"
 
-namespace ZigZag {
-inline __attribute__((always_inline)) uint64_t encode(int64_t val) noexcept {
+namespace BareBones::Encoding::ZigZag {
+
+PROMPP_ALWAYS_INLINE uint64_t encode(int64_t val) noexcept {
   // cppcheck-suppress shiftTooManyBitsSigned
   return (static_cast<uint64_t>(val) + static_cast<uint64_t>(val)) ^ (val >> 63);
 }
 
-inline __attribute__((always_inline)) uint32_t encode(int32_t val) noexcept {
+PROMPP_ALWAYS_INLINE uint32_t encode(int32_t val) noexcept {
   // cppcheck-suppress shiftTooManyBitsSigned
   return (static_cast<uint32_t>(val) + static_cast<uint32_t>(val)) ^ (val >> 31);
 }
 
-inline __attribute__((always_inline)) int64_t decode(uint64_t val) noexcept {
+PROMPP_ALWAYS_INLINE int64_t decode(uint64_t val) noexcept {
   return (val >> 1) ^ (0 - (val & 1));
 }
 
-inline __attribute__((always_inline)) int32_t decode(uint32_t val) noexcept {
+PROMPP_ALWAYS_INLINE int32_t decode(uint32_t val) noexcept {
   return (val >> 1) ^ (0 - (val & 1));
 }
-}  // namespace ZigZag
 
-}  // namespace Encoding
-}  // namespace BareBones
+}  // namespace BareBones::Encoding::ZigZag
